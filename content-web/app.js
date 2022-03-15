d4c88e25a2f9a56c0b49c46171189dfa2fa7fb6b
mickgrice <michael.grice@milliman.com>
2022-03-15 19:49:16 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("6e6403bc-49ff-4132-bee6-af30d5babd09");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
